94ad5cc23ccf20dd67f8bd138994d96a19622471
Brainless18 <alirezadavoodi1378@gmail.com>
2021-03-02 14:34:55 +0330

    lowered email test

--- app/core/models.py
+++ app/core/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
