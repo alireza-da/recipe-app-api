6f3e0e39e8a6b8188e530af1c14537a97ccbdff8
Brainless18 <alirezadavoodi1378@gmail.com>
2021-03-02 14:33:05 +0330

    lowered email test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     def test_create_user_with_email(self):
         # Test creating a user with email is successful
@@ -9,3 +10,9 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email=email, password=password)
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        # Test the user's email is normalized
+        email = 'test@LONDONAPPDEV.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+        self.assertEqual(user.email, email.lower())
