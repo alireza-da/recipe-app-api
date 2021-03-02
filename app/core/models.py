dfa3ab0bb9cc843228561a36f29a647c08988012
Brainless18 <alirezadavoodi1378@gmail.com>
2021-03-02 14:38:34 +0330

    email test

--- app/core/models.py
+++ app/core/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,3 +16,9 @@ class ModelTests(TestCase):
         email = 'test@LONDONAPPDEV.COM'
         user = get_user_model().objects.create_user(email, 'test123')
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        # Test creating user with no email raises error
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+            
