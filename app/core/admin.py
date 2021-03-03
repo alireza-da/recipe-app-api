aa5b6f17080e188c5c00b0df95e03b4f79983825
Brainless18 <alirezadavoodi1378@gmail.com>
2021-03-03 15:37:42 +0330

    admin user

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from app.core import models
+import models
+
 
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
