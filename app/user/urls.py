9b9926c73745ef890d3ccb9827e4d4a5b5dd15b8
Brainless18 <alirezadavoodi1378@gmail.com>
2021-03-07 20:56:48 +0330

    user create api added

---
+++ app/user/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from user import views
+
+app_name = 'user'
+
+url_patterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+]
--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from serializers import UserSerializer
+from rest_framework import generics
+
 
 # Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    # Create a new user in system
+    serializer_class = UserSerializer
