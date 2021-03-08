2f2a0f69f0e063da6000aa9cad4448aa986339a3
Brainless18 <alirezadavoodi1378@gmail.com>
2021-03-08 13:58:02 +0330

    token api test added

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
 
@@ -12,3 +13,29 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         # Create a new user with encrypted password
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    # Serializer authentication object
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        # Validate and authenticate the user
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('context'),
+            username=email,
+            password=password
+        )
+        if not user:
+            message = _("Unable to authenticate with provided credentials")
+            raise serializers.ValidationError(message, code='authentication')
+        attrs['user'] = user
+        return user
+
+
