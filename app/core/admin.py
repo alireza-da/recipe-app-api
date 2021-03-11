7c6fa02cff4ce269e4c3575d4ef6114bec8533d1
Brainless18 <alirezadavoodi1378@gmail.com>
2021-03-11 18:32:15 +0330

    Tag model added

--- app/core/admin.py
+++ app/core/admin.py
@@ -22,3 +22,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Tag)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email="test123@test.com", password="test123"):
+    return get_user_model().objects.create_user(email=email, password=password)
 
 
 class ModelTests(TestCase):
@@ -31,3 +36,11 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        # Test tag string representation
+        tag = models.Tags.objects.create(
+            user=sample_user(),
+            name="Vegan"
+        )
+        self.assertEqual(str(tag), tag.name)
