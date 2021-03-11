c726bc9c791c8f5aa7c76609aaa51e865091ac3e
Brainless18 <alirezadavoodi1378@gmail.com>
2021-03-11 20:32:22 +0330

    Tags retrievation test added

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from core.models import Tag
+from recipe import serializers
 
-# Create your views here.
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    # Manage Tags in the database
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        # return objects for the authenticated user only
+        return self.queryset.filter(user=self.request.user).order_by('-name')
