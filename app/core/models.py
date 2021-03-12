c0365287a030ec616a8620152db9f015096dc839
Brainless18 <alirezadavoodi1378@gmail.com>
2021-03-12 18:53:56 +0330

    Ingredient Model added

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser,\
+from django.contrib.auth.models import AbstractBaseUser, \
     BaseUserManager, PermissionsMixin
 from django.conf import settings
 
@@ -44,3 +44,14 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+
+class Ingredient(models.Model):
+    # Ingredient to be user in a recipe
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -44,3 +44,11 @@ class ModelTests(TestCase):
             name="Vegan"
         )
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        # Test ingredient string representation
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name="Cucumber"
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
