79e429f89ee139d8d87278a1dcee57319e384858
Brainless18 <alirezadavoodi1378@gmail.com>
2021-03-12 13:28:31 +0330

    Create tag function

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -50,3 +50,22 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['name'], tag.name)
+
+    def test_create_tag(self):
+        # Test that tags created successfully
+        payload = {
+            'name': 'test tag'
+
+        }
+        self.client.post(TAGS_URL, payload)
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        # Test that create tag with invalid payload
+        payload = {'name': ''}
+        response = self.client.post(TAGS_URL, payload)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
