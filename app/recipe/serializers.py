ce9fb26107109eab3ce6f2902e349ff00e988492
Brainless18 <alirezadavoodi1378@gmail.com>
2021-03-26 21:33:40 +0430

    Recipe test cases and serializer added

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -27,3 +27,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes', 'price', 'link')
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
