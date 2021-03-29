5daa8c8e8c73cfa6be87fc1fbf522dc0fda78449
Brainless18 <alirezadavoodi1378@gmail.com>
2021-03-29 21:05:17 +0430

    Image path , field feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from core import models
-
+from unittest.mock import patch
 
 def sample_user(email="test123@test.com", password="test123"):
     return get_user_model().objects.create_user(email=email, password=password)
@@ -62,3 +62,12 @@ class ModelTests(TestCase):
             price=5.00
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        # Test that image saved in the correct location
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'myImage.jpg')
+        exp_path = f'upload/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, exp_path)
