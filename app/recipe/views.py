0ff9812bb69d9236f6605725f84b881f0e3dbcc2
Brainless18 <alirezadavoodi1378@gmail.com>
2021-03-31 19:16:16 +0430

    filtering's views and tests added

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -39,7 +39,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def _params_to_ints(self, queryset):
         # Convert a list of string IDs to a list of integers
-        return [int(str_id) for str_id in queryset.split([','])]
+        return [int(str_id) for str_id in queryset]
 
     def get_queryset(self):
         tags = self.request.query_params.get('tags')
